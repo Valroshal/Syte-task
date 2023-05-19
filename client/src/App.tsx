0478f4e5e9d4522c92d8748b4fa802726494cae8
Valtoshal <valerie.kabishcher@gmail.com>
2023-05-19 22:21:19 +0300

    remove git hidden folder

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Val's app client</p>
+      <p>New code</p>
+    </div>
+  );
+}
+
+export default App;
